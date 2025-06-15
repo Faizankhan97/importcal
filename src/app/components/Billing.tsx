ec16f0e6d29d130e887336185b89ffce1a6a1205
Faizan Khan <faizikhan753@gmail.com>
2025-06-15 21:58:56 +0500

    billing

---
+++ src/app/components/Billing.tsx
@@ -0,0 +1,178 @@
+import { Col, Divider, Row } from "antd";
+import check from "../../assets/images/check.png";
+import rightArrow from "../../assets/images/right.png";
+import cross from "../../assets/images/cross.png";
+import React from "react";
+import Image from "next/image";
+
+const Billing = () => {
+  return (
+    <div className="box">
+      <Row className="justify-between">
+        <Col span={6}>
+          <h4>Pricing Plan</h4>
+        </Col>
+        <Col span={6}>
+          <div className="bill">
+            <h6>Bill yearly</h6>
+            <span>-$80</span>
+            <p>Bill monthly</p>
+          </div>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={8} className="gutter-row">
+          <div className="box_line">
+            <div className="flex">
+              <h6>Premium</h6>
+            </div>
+            <h3>
+              $19 <span>user / month</span>
+            </h3>
+            <p>
+              Aenean at lectus posuere enim id nec. Molestie neque, sed fusce
+              faucibus.
+            </p>
+            <Divider />
+            <ul>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={cross} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={cross} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+            </ul>
+            <button>
+              Get started <Image src={rightArrow} alt="arrow" />{" "}
+            </button>
+          </div>
+        </Col>
+        <Col span={8} className="gutter-row">
+          <div className="box_line">
+            <div className="flex justify-between align-middle">
+              <h6>Team</h6>
+              <span className="plan">🔥 Current Plan</span>
+            </div>
+            <h3>
+              $39 <span>user / month</span>
+            </h3>
+            <p>
+              Aenean at lectus posuere enim id nec. Molestie neque, sed fusce
+              faucibus.
+            </p>
+            <Divider />
+            <ul>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+            </ul>
+            <button>
+              Get started <Image src={rightArrow} alt="arrow" />
+            </button>
+          </div>
+        </Col>
+        <Col span={8} className="gutter-row">
+          <div className="box_line">
+            <div className="flex">
+              <h6>Enterprise</h6>
+            </div>
+            <h3 style={{ fontSize: "24px" }}>Custom pricing</h3>
+            <p>
+              Aenean at lectus posuere enim id nec. Molestie neque, sed fusce
+              faucibus.
+            </p>
+            <Divider />
+            <ul>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+              <li>
+                <Image src={check} alt="check" />
+                <span>
+                  Tortor interdum condimentum nunc molestie quam lectus.
+                </span>
+              </li>
+            </ul>
+            <button>
+              Contact us <Image src={rightArrow} alt="arrow" />
+            </button>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default Billing;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,103 @@
+import { PlusOutlined } from "@ant-design/icons";
+import home from "../assets/images/home.png";
+import flowbite from "../assets/images/flowbite.png";
+import fluent from "../assets/images/fluent.png";
+import copy from "../assets/images/Copy.png";
+import bell from "../assets/images/bell.png";
+import settings from "../assets/images/Settings.png";
+import arrow from "../assets/images/arrow.png";
+import info from "../assets/images/info.png";
+import { Divider, Tabs } from "antd";
 import Image from "next/image";
+import Billing from "./components/Billing";
 
 export default function Home() {
+  const items = [
+    {
+      key: "1",
+      label: "Company",
+      children: "",
+    },
+    {
+      key: "2",
+      label: "Billing",
+      children: <Billing />,
+    },
+    {
+      key: "3",
+      label: "Team",
+      children: "",
+    },
+    {
+      key: "4",
+      label: "Notifications",
+      children: "",
+    },
+    {
+      key: "5",
+      label: "API Keys",
+      children: "",
+    },
+  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="main flex">
+      <div className="bg-white left_wrap">
+        <button className="generte">
+          <PlusOutlined />
+          Generate report
+        </button>
+        <div className="flex flex-col justify-between height_wrap">
+          <div>
+            <h4>Your Workspace</h4>
+            <ul>
+              <li className="flex">
+                <Image src={home} alt="home" />
+                <p>My Dashoard</p>
+              </li>
+              <li className="flex">
+                <Image src={copy} alt="copy" />
+                <p>My reports</p>
+              </li>
+            </ul>
+            <Divider />
+            <h4>Coming soon pages </h4>
+            <ul>
+              <li className="flex">
+                <Image src={flowbite} alt="flowbite" />
+                <p>World terif Map</p>
+              </li>
+              <li className="flex">
+                <Image src={fluent} alt="fluent" />
+                <p>Products HTS Cla....</p>
+              </li>
+            </ul>
+            <Divider />
+            <h4>Others</h4>
+            <ul className="border-0">
+              <li className="flex">
+                <Image src={bell} alt="bell" />
+                <p>Notifications</p>
+              </li>
+              <li className="flex">
+                <Image style={{ opacity: 1 }} src={settings} alt="settings" />
+                <p style={{ opacity: 1 }}>Settings</p>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <button className="arrow">
+              <Image style={{ opacity: 1 }} src={arrow} alt="arrow" />
+            </button>
+            <h4 className="help flex">
+              <Image src={info} alt="info" />
+              Help Center
+            </h4>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="right_wrap">
+        <Tabs defaultActiveKey="2" items={items} />
+      </div>
     </div>
   );
 }
